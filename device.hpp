44ec93572482181d764c95485cc73a3987ca9d2a
Tobias Linn <tobias.linn@rwth-aachen.de>
2015-04-22 11:07:33 +0200

    auto->double contacts;;;;;;

--- device.hpp
+++ device.hpp
@@ -10,41 +10,48 @@
 namespace d {
 
     // material properties
-    static constexpr auto eps_c = 11.2;                                             // relative permittivity of channel
-    static constexpr auto eps_o = 12;                                               // relative permittivity of oxide
-    static constexpr auto E_g   = 0.6;                                              // bandgap
-    static constexpr auto m_eff = 0.2 * c::m_e;                                     // effective mass in conduction band
-    static constexpr auto F_s   = -(E_g/2 + 0.0151);                                // Fermi level in source
-    static constexpr auto F_c   = 0;                                                // Fermi level in channel
-    static constexpr auto F_d   = +(E_g/2 + 0.0151);                                // Fermi level in drain
+    static constexpr double eps_c = 11.2;                                         // relative permittivity of channel
+    static constexpr double eps_o = 12;                                           // relative permittivity of oxide
+    static constexpr double E_g   = 0.6;                                          // bandgap
+    static constexpr double m_eff = 0.2 * c::m_e;                                 // effective mass in conduction band
+    static constexpr double F_s   = -(E_g/2 + 0.0151);                            // Fermi level in source
+    static constexpr double F_c   = 0;                                            // Fermi level in channel
+    static constexpr double F_d   = +(E_g/2 + 0.0151);                            // Fermi level in drain
 
     // geometry (everything in nm)
-    static constexpr auto l_c   = 10;                                               // channel length
-    static constexpr auto d_c   = 2;                                                // channel thickness
-    static constexpr auto d_o   = 0.8;                                              // oxide thickness
-    static constexpr auto lam_c = sqrt(eps_c*d_c*d_c/8/eps_o * log(1 + 2*d_o/d_c)); // scr. length in channel
-    static constexpr auto lam_s = 1.0 < lam_c ? 1.0 : lam_c;                        // scr. length in source
-    static constexpr auto lam_d = 1.0 < lam_c ? 1.0 : lam_c;                        // scr. length in drain
-    static constexpr auto l_s   = 15 * lam_s;                                       // source length
-    static constexpr auto l_d   = 15 * lam_d;                                       // drain length
-    static constexpr auto l     = l_s + l_c + l_d;                                  // device length
+    static constexpr double l_c   = 10;                                           // channel length
+    static constexpr double d_c   = 2;                                            // channel thickness
+    static constexpr double d_o   = 0.8;                                          // oxide thickness
+    static constexpr double lam_c = sqrt(eps_c*d_c*d_c/8/eps_o*log(1+2*d_o/d_c)); // scr. length in channel
+    static constexpr double lam_s = 1.0 < lam_c ? 1.0 : lam_c;                    // scr. length in source
+    static constexpr double lam_d = 1.0 < lam_c ? 1.0 : lam_c;                    // scr. length in drain
+    static constexpr double l_s   = 15 * lam_s;                                   // source length
+    static constexpr double l_d   = 15 * lam_d;                                   // drain length
+    static constexpr double l     = l_s + l_c + l_d;                              // device length
 
     // lattice
-    static constexpr auto dx    = 0.1;                                              // lattice constant
-    static constexpr int N_s    = round(l_s / dx);                                  // # of lattice points in source
-    static constexpr int N_c    = round(l_c / dx);                                  // # of lattice points in channel
-    static constexpr int N_d    = round(l_d / dx);                                  // # of lattice points in drain
-    static constexpr auto N_x   = N_s + N_c + N_d;                                  // total # of lattice points
-    static const arma::vec x    = arma::linspace(0.5 * dx, l - 0.5 * dx, N_x);      // lattice points
+    static constexpr double dx    = 0.1;                                          // lattice constant
+    static constexpr int    N_s   = round(l_s / dx);                              // # of lattice points in source
+    static constexpr int    N_c   = round(l_c / dx);                              // # of lattice points in channel
+    static constexpr int    N_d   = round(l_d / dx);                              // # of lattice points in drain
+    static constexpr int    N_x   = N_s + N_c + N_d;                              // total # of lattice points
+    static const arma::vec  x     = arma::linspace(0.5 * dx, l - 0.5 * dx, N_x);  // lattice points
 
     // ranges
-    static const     auto s     = arma::span(0, N_s - 1);
-    static const     auto c     = arma::span(N_s, N_s + N_c - 1);
-    static const     auto d     = arma::span(N_s + N_c, N_s + N_c + N_d - 1);
+    static const     auto   s     = arma::span(0, N_s - 1);                         // source area
+    static const     auto   c     = arma::span(N_s, N_s + N_c - 1);                 // channel area
+    static const     auto   d     = arma::span(N_s + N_c, N_s + N_c + N_d - 1);     // drain area
 
     // hopping parameters
-    static constexpr auto t1    = 0.25 * E_g * (1 + sqrt(1 + 2 * c::h_bar*c::h_bar / (dx*dx * 1E-18 * m_eff * E_g * c::e)));
-    static constexpr auto t2    = 0.25 * E_g * (1 - sqrt(1 + 2 * c::h_bar*c::h_bar / (dx*dx * 1E-18 * m_eff * E_g * c::e)));
+    static constexpr double t1    = 0.25 * E_g * (1 + sqrt(1 + 2 * c::h_bar*c::h_bar / (dx*dx * 1E-18 * m_eff * E_g * c::e)));
+    static constexpr double t2    = 0.25 * E_g * (1 - sqrt(1 + 2 * c::h_bar*c::h_bar / (dx*dx * 1E-18 * m_eff * E_g * c::e)));
+
+    // contacts
+    static constexpr double E_gc  = 0.3;
+    static constexpr double m_efc = 0.1 * c::m_e;
+    static constexpr double tc1   = 0.25 * E_gc * (1 + sqrt(1 + 2 * c::h_bar*c::h_bar / (dx*dx * 1E-18 * m_efc * E_gc * c::e)));
+    static constexpr double tc2   = 0.25 * E_gc * (1 + sqrt(1 + 2 * c::h_bar*c::h_bar / (dx*dx * 1E-18 * m_efc * E_gc * c::e)));
+    static constexpr double tcn   = -0.2;
 
     // off diagonal of hamiltonian
     template<int N>
@@ -65,9 +72,9 @@ namespace d {
     static const     auto t_vec     = create_t_vec<N_x>();
 
     // integration parameters
-    static constexpr auto E_min = -1.5;
-    static constexpr auto E_max = +1.5;
-    static constexpr auto rel_tol = 1e-3;
+    static constexpr double E_min = -1.5;
+    static constexpr double E_max = +1.5;
+    static constexpr double rel_tol = 1e-3;
 
     // doping
     inline arma::vec create_n0() {
